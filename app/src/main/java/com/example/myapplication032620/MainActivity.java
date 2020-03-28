cde3f93b0716b53b562a526b15ddc496e355debc
Vasile <vaseoa@yahoo.com>
2020-03-28 10:05:37 +0200

    inceputul zilei

--- app/src/main/java/com/example/myapplication032620/MainActivity.java
+++ app/src/main/java/com/example/myapplication032620/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.support_simple_spinner_dropdown_item);
 	}
 }
