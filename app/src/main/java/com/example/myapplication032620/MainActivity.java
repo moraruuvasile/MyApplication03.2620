6c9fba34555bc3a00214a14bfb0dec447446f849
Vasile <vaseoa@yahoo.com>
2020-03-30 00:22:15 +0300

    layout finish

--- app/src/main/java/com/example/myapplication032620/MainActivity.java
+++ app/src/main/java/com/example/myapplication032620/MainActivity.java
@@ -16,36 +16,12 @@ public class MainActivity extends AppCompatActivity {
 	private static final String TAG = "MainActivity";
 	private final String SAVED_STATE = "Saved State";
 
-	@Override
-	protected void onSaveInstanceState(@NonNull Bundle outState) {
-		outState.putString(SAVED_STATE, textView.getText().toString());
-		super.onSaveInstanceState(outState);
-	}
-
-	@Override
-	protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-		super.onRestoreInstanceState(savedInstanceState);
-		textView.setText(savedInstanceState.getString(SAVED_STATE));
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Log.d(TAG, "onCreate: Start");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		Button button = findViewById(R.id.button);
-		EditText editText = findViewById(R.id.editText3);
-		textView = findViewById(R.id.textView);
-		textView.setMovementMethod(new ScrollingMovementMethod());
-		textView.setText("");
-		editText.setText("");
-		button.setOnClickListener(view -> {
-			String text = editText.getText().toString();
-			textView.append(text + "\n");
-			editText.setText("");
-			Log.d(TAG, "onCreate: Finish");
-		});
 
 
 	}
